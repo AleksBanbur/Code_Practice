2064447a1e4e2f37a3dad5d0187b64b56d4e7ddd
Aleksander Banbur <44559788+AleksBanbur@users.noreply.github.com>
2022-10-09 23:06:29 -0400

    Create io_num.py

---
+++ Practice_Problems/io_num.py
@@ -0,0 +1,15 @@
+#Problem
+#Problem Statement
+#Write a program that accepts a number, n, and outputs the same.
+
+#Input
+#The only line contains a single integer.
+
+#Output
+#Output the answer in a single line.
+
+#Constraints
+#0 ≤ n ≤ 10^5
+
+num = int(input())
+print(num)
